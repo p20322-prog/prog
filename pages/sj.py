0ee1dc1b89f851b3e7a584711af8f35f6c5136f9
p20322-prog <p20322@sw.hs.kr>
2026-01-15 12:26:49 +0900

    Update sj.py

--- pages/sj.py
+++ pages/sj.py
@@ -97,7 +97,7 @@ def empathic_response(user_input):
 print("공감형 AI입니다. '종료'라고 입력하면 끝나요.")
 
 while True:
-    user_input = input("나: ")
+    user_input = st.textinput("나: ")
 
     if "종료" in user_input:
         total = sum(emotion_count.values())
