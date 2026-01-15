6a3be0a7de2ecdb62d741364f8179e087d99c2d5
p20322-prog <p20322@sw.hs.kr>
2026-01-15 12:28:19 +0900

    Update sj.py

--- pages/sj.py
+++ pages/sj.py
@@ -94,10 +94,10 @@ def empathic_response(user_input):
     ])
 
 # 메인 대화 루프
-print("공감형 AI입니다. '종료'라고 입력하면 끝나요.")
+st.write("공감형 AI입니다. '종료'라고 입력하면 끝나요.")
 
 while True:
-    user_input = st.textinput("나: ")
+    user_input = st.text_input("나: ")
 
     if "종료" in user_input:
         total = sum(emotion_count.values())
@@ -105,17 +105,17 @@ while True:
 
 
         if total == 0:
-            print(" 아직 감정이 뚜렷하게 드러나진 않았어.")
+            st.write(" 아직 감정이 뚜렷하게 드러나진 않았어.")
         else:
             for emotion, count in emotion_count.items():
                 if count > 0:
                     percent = round((count / total) * 100, 1)
-                    print(f"AI: {emotion}이(가) 약 {percent}% 정도 느껴졌어.")
-            print("\nAI: 지금까지의 대화를 바탕으로 보면,")
-            print("    이건 판단이 아니라, 네가 표현해 온 감정의 흐름이야.")
-            print("    이야기해 줘서 고마워. 언제든 다시 와 😊")
+                    st.write(f"AI: {emotion}이(가) 약 {percent}% 정도 느껴졌어.")
+            st.write("\nAI: 지금까지의 대화를 바탕으로 보면,")
+            st.write("    이건 판단이 아니라, 네가 표현해 온 감정의 흐름이야.")
+            st.write("    이야기해 줘서 고마워. 언제든 다시 와 😊")
         break
 
     response = empathic_response(user_input)
-    print("AI:", response)
+    st.write("AI:", response)
 
